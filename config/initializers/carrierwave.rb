900cf0bf66cda23f55db07e661b58ed6114630f6
Sanderson Santana <sanderson@digaeita.com.br>
2013-01-29 12:44:21 -0300

    carrier wave configs

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAIWJDB4QUSTONFLIA',                        # required
-    :aws_secret_access_key  => '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',                        # required
+    :aws_access_key_id      => ENV['S3_ACCESS_KEY_ID'],                        # required
+    :aws_secret_access_key  => ENV['S3_SECRET_ACCESS_KEY'],                        # required
   }
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
